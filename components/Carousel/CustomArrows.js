78bf0fefd6db0012bb1cba887d75ae86e350ebf1
Piyush Bora <piyush.bora2000@gmail.com>
2023-10-13 00:23:15 +0530

    still slider was bad, there were issues of autoplay, so replaced the whole carousel with swiper slider

--- components/Carousel/CustomArrows.js
+++ components/Carousel/CustomArrows.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import styles from './Carousel.module.css';
 import {AiOutlineArrowRight, AiOutlineArrowLeft} from 'react-icons/ai';
-import { useSwiper } from 'swiper/react';
-
-const swiper = useSwiper();
 
 const CustomArrows = () => (
-  <><button onClick={() => swiper.slidePrev()} className={`${styles['swiper-button-prev']}`}>
-    <AiOutlineArrowLeft/>
-  </button>
-  <button onClick={() => swiper.slideNext()} className={`${styles['swiper-button-next']}`}>
-    <AiOutlineArrowRight/>
-  </button></>
+  <></>
 );
 
 
